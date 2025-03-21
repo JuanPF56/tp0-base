5255420a9f455e9f480e89684ea0ba14cd5d8dba
JuanPF56 <jpfresia@fi.uba.ar>
2025-03-21 02:55:26 -0300

    fix: correct netcat command syntax in echo server validation script

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -11,7 +11,7 @@ server_port=12345
 docker run --rm --network $network_name alpine sh -c "
     apk add --no-cache netcat-openbsd
     
-    response=\$(echo $test_message | nc -w $timeout -s $server_ip -p $server_port)
+    response=\$(echo $test_message | nc -w $timeout $server_ip $server_port)
 
     if
         [ \"\$response\" != \"$test_message\" ];
