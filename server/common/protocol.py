23dd7fb2e59b21137df74064c0310e9291d54141
JuanPF56 <jpfresia@fi.uba.ar>
2025-03-25 07:03:53 -0300

    fix: correct logging level typo

--- server/common/protocol.py
+++ server/common/protocol.py
@@ -55,7 +55,7 @@ class Protocol:
             length += field_length
             start += 2 + field_length
 
-        logging.DEBUG(f"action: receive_message | result: success | fields: {fields}")
+        logging.debug(f"action: receive_message | result: success | fields: {fields}")
 
         if len(fields) != 6 or length != total_length:
             raise ValueError("invalid message format")
