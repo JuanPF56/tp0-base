63d7c7744ab2bfb6568765edde0982c722caf994
JuanPF56 <jpfresia@fi.uba.ar>
2025-03-25 07:06:39 -0300

    fix: remove debug logging from receive_message method

--- server/common/server.py
+++ server/common/server.py
@@ -50,7 +50,6 @@ class Server:
         """
         try:
             bet = self.protocol.parseBetMessage(client_connection.recvMsg())
-            logging.DEBUG(f"action: receive_message | result: success | agency: {bet.agency} | dni: {bet.document} | number: {bet.number}")
             success = self._store_bet(bet)
             client_connection.sendMsg(self.protocol.createResponse(success))
         except OSError as e:
