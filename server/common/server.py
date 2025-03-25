6263bda32eb2ef33b1be52f03d3978aa7bd33a87
JuanPF56 <jpfresia@fi.uba.ar>
2025-03-25 06:59:38 -0300

    fix: update logging in receive_message

--- server/common/server.py
+++ server/common/server.py
@@ -50,7 +50,7 @@ class Server:
         """
         try:
             bet = self.protocol.parseBetMessage(client_connection.recvMsg())
-            logging.DEBUG(f"action: receive_message | result: success | agency: {bet.agency} | name: {bet.name} | surname: {bet.surname} | document: {bet.document} | birthdate: {bet.birthdate} | number: {bet.number}")
+            logging.DEBUG(f"action: receive_message | result: success | agency: {bet.agency} | dni: {bet.document} | number: {bet.number}")
             success = self._store_bet(bet)
             client_connection.sendMsg(self.protocol.createResponse(success))
         except OSError as e:
